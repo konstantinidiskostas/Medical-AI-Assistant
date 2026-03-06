35e759334226e962b7d399553c09e93299344346
cycloholic <konstantinidis.kon@yahoo.com>
2026-03-06 11:35:57 +0100

    Configure pom.xml with Spring Boot starters and initial project setup.

--- src/main/java/com/medical/ai/MedicalAIApplication.java
+++ src/main/java/com/medical/ai/MedicalAIApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//comment
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class MedicalAIApplication {
